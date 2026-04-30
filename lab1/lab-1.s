92bb9efdb48056e137676c18ec60fda035d686e7
josan07 <155398390+josan07@users.noreply.github.com>
2026-04-30 14:32:41 +0100

    Update lab-1.s

--- lab1/lab-1.s
+++ lab1/lab-1.s
@@ -4,22 +4,22 @@ b: .half -14
 c: .byte 4
 res: .zero 64
 
+
+# (a*c + b) / 2
 .text
 
-la x7, res
-ld x7,0(x7)
+la x10, a
+lw x5, 0(x10)
+lh x6, 4(x10)
+lb x7, 6(x10)
+lw x8, 7(x10)
 
-lw x6,a
+
+mul x5, x5, x7
 add x5, x5, x6
+srli x5 , x5 , 1
 
-lb x6, c
-mul x5, x5, x6
 
-lh x6, b
-add, x5, x5, x6
-
-srli, x5, x5, 1
-
-sw x5, 0(x7)
+sw x5, 0(x10)
 
 loop: j loop

ba5e6ffecad9cdc969442fdeedc7cc5fbc2751fd
josan07 <155398390+josan07@users.noreply.github.com>
2026-05-05 09:37:30 +0100

    Refactor vector initialization and loop for summation
    Updated vector and N values, modified loop logic for summation.

--- lab2/prep-1.s
+++ lab2/prep-1.s
@@ -1,31 +1,30 @@
 .data
-vector: .word 10,20
-N: .word 2
+vector: .word 10, 20, 30, 40
+N: .word 4
+
 
 .text
+la x5 vector   # x5 endereço vector
 
-la x12 vector    # x5 vector 
+li x6 0        # x6 init soma
+li x7 0        # x7 init contador
 
-la x10 N
-lw x10 0(x10)   # x10 N
+lw x8 N        # x8 N
 
-li x6 0         # x6 soma
-li x7 0         # x7 contador
+mv x9 x5       # x9 copia endereço vector
 
-mv x8 x12        # copiar endereço vector
-
-loop:
-    lw x9 0(x8)
-    add x6 x6 x9
+vector_loop:
+    lw x10 0(x9)
+    
+    add x6 x6 x10
     addi x7 x7 1
     
+    beq x7 x8 end_loop
     
-    addi x8 x8 4
-    beq x7 x10 end_loop
+    addi x9 x9 4
     
-    j loop   
-    
+    j vector_loop
+
 end_loop:
     div x6 x6 x7
     
-

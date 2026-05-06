e76b011362a8c909cf900a40e9060b20ee41e530
josan07 <155398390+josan07@users.noreply.github.com>
2026-05-07 00:16:10 +0100

    Create prep-5-1.s

---
+++ lab2/prep-5-1.s
@@ -0,0 +1,37 @@
+.data
+SwitchesBase:   .word SWITCHES_0_BASE
+LEDMatrixBase: .word LED_MATRIX_0_BASE
+LEDMatrixWidth: .byte LED_MATRIX_0_WIDTH
+LEDMatrixHeight: .byte LED_MATRIX_0_HEIGHT
+
+
+.text
+la x5, SwitchesBase
+lwu x5, 0(x5)
+
+andi x6 x5 0b0001   # x6=1 desloca, =0 stop
+
+la x7, LEDMatrixBase
+lwu x7, 0(x7)
+mv x11 x7
+
+
+la x8, LEDMatrixWidth
+lbu x8, 0(x8)
+
+la x9, LEDMatrixHeight
+lbu x9, 0(x9)
+
+li x10 0x00ffff00 #yellow
+li x12 0x00000000 #blk
+
+loop:
+sw x10 0(x11)
+addi x11 x11 4 
+sw x12 -4(x11)
+j loop 
+
+
+    
+    
+
